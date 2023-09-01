e00658f9f8a435ef3c3d5690ccc071c1d909d0eb
gplay <gplay8440@gmail.com>
2023-09-01 19:32:45 +0700

    Second commit

--- Lesson_003/00_bubbles.py
+++ Lesson_003/00_bubbles.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
-
+import random
+import time
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
+def draw_buble(position, radius=50, step=5, color=sd.COLOR_GREEN, width=1):
+
+    for y in range(3):
+        sd.circle(center_position=position, radius=radius, color=color, width=width)
+        radius += step
+
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+buble_position = sd.get_point(300, 300)
+draw_buble(position=buble_position, radius=50)
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
